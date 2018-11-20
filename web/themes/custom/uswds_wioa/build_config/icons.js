117e62e9e9fa6e72176d2d74d03117be6de1e73e
James Mejia <s.jamesmejia@gmail.com>
2018-11-20 14:31:11 -0600

    Formatting updates to gulp tasks and add icon task.

---
+++ web/themes/custom/uswds_wioa/build_config/icons.js
@@ -0,0 +1,37 @@
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const iconfont = require("gulp-iconfont");
+const iconfontCSS = require("gulp-iconfont-css");
+
+const runTimestamp = Math.round(Date.now() / 1000);
+const fontName = "wioa-icons";
+
+gulp.task("optimize-images", () => {
+  return gulp.src("./images/**/*", { base: "." }).pipe(imagemin());
+});
+
+gulp.task("iconfont", () => {
+  return gulp
+    .src("./images/svg/*.svg")
+    .pipe(
+      iconfontCSS({
+        fontName: fontName,
+        path: "./sass/templates/icons.scss",
+        targetPath: "../../../sass/global/_icons.scss",
+        fontPath: "../assets/dist/fonts/",
+        cacheBuster: runTimestamp
+      })
+    )
+    .pipe(
+      iconfont({
+        fontName: fontName,
+        // Remove woff2 if you get an ext error on compile
+        formats: ["svg", "ttf", "eot", "woff", "woff2"],
+        normalize: true,
+        fontHeight: 1001,
+        prependUnicode: true,
+        timestamp: runTimestamp
+      })
+    )
+    .pipe(gulp.dest("./assets/dist/fonts/"));
+});

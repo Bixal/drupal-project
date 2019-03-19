eb5bd027070a6b1a9848d6187e53190e4c5b87be
Matt Poole <matt.poole@bixal.com>
2019-03-19 13:59:03 -0400

    WIOA-389: Added custom entities retrieval service to taxonomy service. Added methods to retrieve vocabs as nested structures and to add section display information to the terms.

--- web/modules/custom/sp_retrieve/src/TaxonomyService.php
+++ web/modules/custom/sp_retrieve/src/TaxonomyService.php
@@ -3,6 +3,8 @@
 namespace Drupal\sp_retrieve;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\sp_create\PlanYearInfo;
 
 /**
  * Class CustomEntitiesService.
@@ -17,13 +19,23 @@ class TaxonomyService {
   protected $entityTypeManager;
 
   /**
+   * Retrieve custom entities.
+   *
+   * @var \Drupal\sp_retrieve\CustomEntitiesService
+   */
+  protected $customEntitiesRetrieval;
+
+  /**
    * Constructs a new TaxonomyService object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Entity type manager.
+   * @param \Drupal\sp_retrieve\CustomEntitiesService $custom_entities_retrieval
+   *   Retrieve custom entities.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, CustomEntitiesService $custom_entities_retrieval) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->customEntitiesRetrieval = $custom_entities_retrieval;
   }
 
   /**
@@ -123,6 +135,131 @@ class TaxonomyService {
       }
     }
     return $nested;
+  }
+
+  /**
+   * Retrieve pertinent info to identify state plan content creation info.
+   *
+   * Each section term can be used to create many, none, or one piece of state
+   * plan year content.
+   *
+   * @param \Drupal\taxonomy\Entity\Term $section_year_term
+   *   A section year term.
+   *
+   * @return array
+   *   An array with common elements in the top and individual items in the
+   *   content key.
+   */
+  public function getStatePlanYearContentInfoFromSectionYearTerm(Term $section_year_term) {
+    $plan_year_id_and_section_id = PlanYearInfo::getPlanYearIdAndSectionIdFromVid($section_year_term->bundle());
+    $return['plan_year_id'] = !empty($plan_year_id_and_section_id['plan_year_id']) ? $plan_year_id_and_section_id['plan_year_id'] : '';
+    $return['section_id'] = !empty($plan_year_id_and_section_id['section_id']) ? $plan_year_id_and_section_id['section_id'] : '';
+    $return['content'] = [];
+    if (!$section_year_term->get('field_input_from_state')->isEmpty()) {
+      /** @var \Drupal\sp_field\Plugin\Field\FieldType\SectionEntryItem $item */
+      foreach ($section_year_term->get('field_input_from_state') as $item) {
+        $value = $item->getValue();
+        $return['content'][$value['term_field_uuid']] = $value;
+
+      }
+    }
+    return $return;
+  }
+
+  /**
+   * Retrieve pertinent info to display a section term in a plan year.
+   *
+   * @param \Drupal\taxonomy\Entity\Term $section_year_term
+   *   A section year term.
+   *
+   * @return array
+   *   An array of fields that can be used to display a section year term.
+   */
+  public function getDisplayInfoFromSectionYearTerm(Term $section_year_term) {
+    $plan_year_id_and_section_id = PlanYearInfo::getPlanYearIdAndSectionIdFromVid($section_year_term->bundle());
+    if (FALSE === $plan_year_id_and_section_id) {
+      return [];
+    }
+    $return = $this->getStatePlanYearContentInfoFromSectionYearTerm($section_year_term);
+    $return['tid'] = $section_year_term->id();
+    // Don't show the number nor the formatting. If a section is referenced
+    // by a term that has it's name hidden, the top level term will start at
+    // the hierarchical heading of this term.
+    $return['hide_name'] = $section_year_term->get('field_hide_name')->getString();
+    // Don't show the number but it takes up the same place with the same
+    // formatting.
+    $return['hide_hierarchical_heading'] = !empty($return['hide_name']) || $section_year_term->get('field_hide_hierarchical_heading')->getString();
+    $return['shown_on_toc'] = $section_year_term->get('field_hide_name')->getString();
+    $return['section_references'] = [];
+    $return['weight'] = $section_year_term->getWeight();
+    $return['tid'] = $section_year_term->id();
+    $return['name'] = $section_year_term->get('field_display_name')->isEmpty() ? $section_year_term->getName() : $section_year_term->get('field_display_name')->getString();
+    if (!$section_year_term->get('field_input_from_state')->isEmpty()) {
+      /** @var \Drupal\sp_field\Plugin\Field\FieldType\SectionEntryItem $item */
+      foreach ($section_year_term->get('field_input_from_state') as $item) {
+        $value = $item->getValue();
+        if (empty($value['section'])) {
+          continue;
+        }
+        $return['section_references'][$value['term_field_uuid']] = $value['section'];
+      }
+    }
+    return $return;
+  }
+
+  /**
+   * Take an entire plan year and add display info to each term.
+   *
+   * @param string $plan_year_id
+   *   A plan year ID.
+   *
+   * @return array
+   *   An array of section plan year display info keyed by section ID.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getPlanYearDisplayInfo($plan_year_id) {
+    $return = [];
+    /** @var \Drupal\sp_plan_year\Entity\PlanYearEntity $plan_year */
+    $plan_year = $this->customEntitiesRetrieval->single('plan_year', $plan_year_id);
+    if (NULL === $plan_year) {
+      return $return;
+    }
+    foreach ($plan_year->getSections() as $section) {
+      $nested_sorted_terms = $this->getNestedSortedTerms(
+        PlanYearInfo::createSectionVocabularyId($plan_year->id(), $section->id())
+      );
+      $return[$section->id()] = $this->getSectionVocabDisplayInfo($nested_sorted_terms);
+    }
+    return $return;
+  }
+
+  /**
+   * Retrieve the plan year section display info.
+   *
+   * @param array $nested_sorted_terms
+   *   Takes the results of self::getNestedSortedTerms() and adds more
+   *   information to it for display purposes.
+   *
+   * @return array
+   *   The display info as a nested array for a section.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  protected function getSectionVocabDisplayInfo(array $nested_sorted_terms) {
+    $return = [];
+    foreach ($nested_sorted_terms as $nested_sorted_term) {
+      /** @var \Drupal\taxonomy\Entity\Term $term */
+      $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($nested_sorted_term['tid']);
+      $display_info = $this->getDisplayInfoFromSectionYearTerm($term);
+      if (!empty($nested_sorted_term['children'])) {
+        $display_info['children'] = $this->getSectionVocabDisplayInfo($nested_sorted_term['children']);
+      }
+      $return[] = $display_info;
+    }
+    return $return;
   }
 
 }

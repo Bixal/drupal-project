51347282e6c39e4fdac119a2784fa516e1571502
Matt Poole <matt.poole@bixal.com>
2019-03-28 10:28:19 -0400

    WIOA-427: Dev's UUID implementation is different but they implement the same interface, use the interface so it is more supported.

--- web/modules/custom/sp_field/src/Plugin/Field/FieldWidget/UuidAdditionalDefaultWidget.php
+++ web/modules/custom/sp_field/src/Plugin/Field/FieldWidget/UuidAdditionalDefaultWidget.php
@@ -7,7 +7,7 @@ use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Component\Uuid\Php;
+use Drupal\Component\Uuid\UuidInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -34,7 +34,7 @@ class UuidAdditionalDefaultWidget extends WidgetBase implements ContainerFactory
   /**
    * {@inheritdoc}
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, Php $uuid) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, UuidInterface $uuid) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
     $this->uuid = $uuid;
   }

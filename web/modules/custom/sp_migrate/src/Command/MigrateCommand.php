b7ed6ec4164754a5af4ae10630af01b22409442e
Gerardo Maldonado <gerardo.maldonado@gmail.com>
2019-04-30 22:31:59 -0400

    WIOA-436: Refactored to use dependency injected database service and queryBuilder function

--- web/modules/custom/sp_migrate/src/Command/MigrateCommand.php
+++ web/modules/custom/sp_migrate/src/Command/MigrateCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Core\Command\ContainerAwareCommand;
 use Drupal\Console\Annotations\DrupalCommand;
+use Drupal\Core\Database\Connection;
 
 /**
  * Class MigrateCommand.
@@ -18,6 +19,27 @@ use Drupal\Console\Annotations\DrupalCommand;
 class MigrateCommand extends ContainerAwareCommand {
 
   /**
+   * The database connection needed to query the database.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+
+  protected $database;
+
+  /**
+   * Constructs a new MyHandler.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The logger interface.
+   */
+  public function __construct(Connection $database = NULL) {
+    parent::__construct();
+    if ($database != NULL) {
+      $this->database = $database;
+    }
+  }
+
+  /**
    * {@inheritdoc}
    */
   protected function configure() {
@@ -26,17 +48,17 @@ class MigrateCommand extends ContainerAwareCommand {
       ->setDescription($this->trans('commands.sp_migrate.migrate.description'));
   }
 
- /**
-  * {@inheritdoc}
-  */
+  /**
+   * {@inheritdoc}
+   */
   protected function initialize(InputInterface $input, OutputInterface $output) {
     parent::initialize($input, $output);
     $this->getIo()->info('initialize');
   }
 
- /**
-  * {@inheritdoc}
-  */
+  /**
+   * {@inheritdoc}
+   */
   protected function interact(InputInterface $input, OutputInterface $output) {
     $this->getIo()->info('interact');
   }
@@ -47,5 +69,34 @@ class MigrateCommand extends ContainerAwareCommand {
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->getIo()->info('execute');
     $this->getIo()->info($this->trans('commands.sp_migrate.migrate.messages.success'));
+    var_dump($this->queryBuilder('ak', '2018', '33121',
+                                '2001', '000030275'));
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function queryBuilder($state, $year, $grant_award_id, $form_id, $row) {
+    $query = $this->database->query(
+      'SELECT narr_text 
+              FROM usp_answer_narr 
+              WHERE
+                state = :value1 AND 
+                year = :value2 AND 
+                grant_award_id = :value3 AND 
+                form_id = :value4 AND 
+                row = :value5',
+      array(
+        ':value1' => $state,
+        ':value2' => $year,
+        ':value3' => $grant_award_id,
+        ':value4' => $form_id,
+        ':value5' => $row,
+      )
+    );
+    $data = $query->fetchField();
+
+    return $data;
+  }
+
 }
